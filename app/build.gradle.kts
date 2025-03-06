94275744edf4580f26b85b3ba26f311979b55795
Pizza67 <massimo.messore@gmail.com>
2025-03-06 16:46:55 +0100

    Enabled minification for release builds

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,12 +14,11 @@ android {
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
-
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
